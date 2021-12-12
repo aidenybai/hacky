a5eb54ebeda58860464a7c5f64129dc69758d304
Aiden Bai <aiden.bai05@gmail.com>
2021-12-11 20:18:43 -0800

    Remove extra dep

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
 export default {
   root: 'dev',
-  esbuild: {
-    jsxFactory: 'jsx',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { jsx, jsxs, Fragment } from 'million/jsx-runtime';`,
-  },
 };
